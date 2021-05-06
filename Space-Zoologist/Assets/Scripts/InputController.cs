fa295c7f786e3139f9121baf3c2e7ff258f8ebc3
Steven Tan <steventangerine@gmail.com>
2021-05-06 09:58:33 -0700

    Add

---
+++ Space-Zoologist/Assets/Scripts/InputController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class InputController : MonoBehaviour
+{
+    [SerializeField] string OwnerID;
+    [SerializeField] GameObject InputField;
+    [SerializeField] GameObject Initial;
+    [SerializeField] int selectPos;
+    [SerializeField] int selectEndPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadText(Initial);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectPos = InputField.GetComponent<TMP_InputField>().selectionAnchorPosition;
+        selectEndPos = InputField.GetComponent<TMP_InputField>().selectionFocusPosition;
+    }
+    public void SaveInput(string s)
+    {
+        if(OwnerID == null)
+        {
+            Debug.LogError("null OwnerID");
+        }
+        else
+        {
+           // Debug.Log("OwnerID: "+ OwnerID);
+            //Debug.Log("Message: " + s);
+            PlayerPrefs.SetString(OwnerID, s);
+        }
+    }
+    public void LoadText(GameObject Button)
+    {
+        this.OwnerID = Button.name;
+       // Debug.Log("load OwnerID: " + OwnerID);
+        if(InputField != null)
+        {
+            InputField.GetComponent<TMP_InputField>().text = PlayerPrefs.GetString(OwnerID);
+        }
+        
+    }
+
+    public void ChangeSelectionColor()
+    {
+        var SP = selectPos;
+        var SEP = selectEndPos;
+        if (SEP < SP)
+        {
+            var temp = SP;
+            SP = SEP;
+            SEP = temp;
+        }
+        if(selectEndPos != selectPos)
+        {
+            var OriginalText = InputField.GetComponent<TMP_InputField>().text;
+            var SelectedText = OriginalText.Substring(SP, SEP - SP);
+            Debug.Log(SelectedText);
+
+        }
+
+    }
+}
---
+++ Space-Zoologist/Assets/Scripts/LeftTabGroup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LeftTabGroup : MonoBehaviour
+{
+
+
+    public List<LeftTabButton> tabButtons;
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+
+    public LeftTabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+    public void Subscribe(LeftTabButton button){
+        if(tabButtons == null)
+        {
+            tabButtons = new List<LeftTabButton>();
+        }
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(LeftTabButton button)
+    {
+        ResetTabs();
+        if(selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+        }
+        
+    }
+    public void OnTabExit(LeftTabButton button)
+    {
+        ResetTabs();
+    }
+    public void OnTabSelected(LeftTabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.background.sprite = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for(int i =0; i < objectsToSwap.Count; i++){
+            if (i == index){
+                objectsToSwap[i].SetActive(true);
+            }
+            else{
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(LeftTabButton button in tabButtons)
+        {
+            if(selectedTab!=null && button == selectedTab)
+            {
+                continue;
+            }
+            button.background.sprite = tabIdle;
+        }
+    }
+
+}
